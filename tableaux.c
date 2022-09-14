1e7fff4d7427ae9e0cd72b41e22c8436251399cb
ZAOUI Imad Eddine <112819421+mrImad007@users.noreply.github.com>
2022-09-14 16:56:27 +0100

    Update tableaux.c

--- tableaux.c
+++ tableaux.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void TriCroissante(float *Z, int N){
+void TriDecroissant(float *Z, int N){
 int i,j;
 float a;
  for(i=0;i<N;i++){
     for(j=0;j<N;j++){
-        if(*(Z+i)<*(Z+j)){
+        if(*(Z+i)>*(Z+j)){
             a=*(Z+i);
             *(Z+i)=*(Z+j);
             *(Z+j)=a;
@@ -20,6 +20,51 @@ float a;
     printf("\n");
 }
 
+//tri par selection
+void TriSelection(float *Z,int N){
+    int i,j,index;
+    int tmp=0;
+ for(i=0;i<N;i++){
+    index=i;
+ for (j=i + 1; j < N; j++)
+    {
+      if (*(Z+index) > *(Z+j))
+     index=j;
+
+      if(index!=i){
+        tmp=*(Z+j);
+        *(Z+j)=*(Z+index);
+        *(Z+index)=tmp;
+             }
+      }}
+      printf("le tableau par ordre croissant METHODE PAR SELECTION \n");
+        for(i=0;i<=N-1;i++){
+            printf("%.2f \t",*(Z+i));
+
+        }
+
+}
+        
+//Tri bulle
+void TriBulle(float *Z, int N){
+int i,j,tmp;
+tmp=0;
+for(i=0;i<=N-1;i++){
+    for(j=i+1;j<=N-1;j++){
+            if(*(Z+j)>*(Z+j)){
+               tmp=*(Z+i);
+               *(Z+i)=*(Z+j+1);
+               *(Z+j+1)=tmp;
+            }
+    }
+}
+printf("\n le tableau par ordre croissant METHODE A BULLE \n");
+        for(j=0;j<=N-1;j++){
+            printf("%.2f \t",*(Z+j));
+
+        }
+}   
+     
 float moy(float *Z, int N){
 	int i;
     float S=0,M;
@@ -61,6 +106,9 @@ int main(void)
 	printf("La moyenne de la classe est acceptable. \n");
 	else 
 	printf("La moyenne de la classe est exellente.\n");
-	TriCroissante(Z,N);
+	TriDecroissant(Z,N);
+	TriSelection(Z,N);
+	TriBulle(Z,N);
+	
 	return 0;
 }
