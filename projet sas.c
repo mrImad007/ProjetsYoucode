994a22d853126b3bb0280d347a1fa8ac4dfc8159
ZAOUI Imad Eddine <112819421+mrImad007@users.noreply.github.com>
2022-09-22 12:00:30 +0100

    Update projet sas.c

--- projet sas.c
+++ projet sas.c
@@ -3,22 +3,22 @@
 #include<string.h>
 #include<time.h>
 
-int i,k,n,m,choix1,choix2,code,qtt,Nqtt;
-int nbr=0,j=0;
+int i,j,k,nbr,code,c=0;
 
 typedef struct medicament {
 	 char nom[50];
 	 int codeBarre;
 	 int qtt;
+	 int qttVendu;
 	 float prix;
 	 float ttc;
 	 
 }med;
 
 //fonction de saisi
-med* saisi (med *p,int m,int n){
+med* saisi (med *p,int m,int c){
 
-	for (i=m;i<n;i++){
+	for (i=m;i<c;i++){
 		
 		printf("\nEntrer le nom du produit %d : ",i+1);
 		scanf("%s",p[i].nom);
@@ -36,10 +36,10 @@ med* saisi (med *p,int m,int n){
 }
 
 //Affichage du stock
-void stock (med *p,int n){
+void stock (med *p,int c){
 	
 	printf("Voici les produits valables au stock : \n");
-	for (i=0;i<n;i++){
+	for (i=0;i<c;i++){
 		printf("Nom du produit %d : %s \n",i+1,p[i].nom);
 		printf("Code du produit %d : %d \n",i+1,p[i].codeBarre);
 		printf("Quantite du produit %d : %d \n",i+1,p[i].qtt);
@@ -50,11 +50,11 @@ void stock (med *p,int n){
 }
 
 //sortPrix
-void sortPrix(med *p, int n){
+void sortPrix(med *p, int c){
 float tmp;
 tmp=0;
-for(i=0;i<n-1;i++){
-    for(j=0;j<=n-i-1;j++){
+for(i=0;i<c-1;i++){
+    for(j=0;j<=c-i-1;j++){
             if(p[i].prix>p[j+1].prix){
                tmp=p[j].prix;
                p[j].prix=p[j+1].prix;
@@ -62,16 +62,16 @@ for(i=0;i<n-1;i++){
             }
     }
 }
-stock(p,n);
+stock(p,c);
 }
 
 //SortAlpha
-void sortAlpha (med *p,int n){
+void sortAlpha (med *p,int c){
 	med let;
     int i,j;
-    for(i=0;i<n;i++)
+    for(i=0;i<c;i++)
         {
-            for( j=0;j<n-1;j++)
+            for( j=0;j<c-1;j++)
             {
                 if(strcmp(p[j].nom,p[j+1].nom)==1)
                 {
@@ -82,51 +82,50 @@ void sortAlpha (med *p,int n){
             }
 
         }
-        stock(p,n);
+        stock(p,c);
 }
 
 //Recherche par code
-med* codeResearch(int code,med *p, int n){
-    for(i=0;i<n;i++){
+void codeResearch(int code,med *p, int c){
+    for(i=0;i<c;i++){
         if(code==p[i].codeBarre){
         	printf("Nom du produit : %s \n",p[i].nom);
 		    printf("Quantite du produit : %d \n",p[i].qtt);
 		    printf("Prix du produit : %.2f \n",p[i].prix);
-		    printf("Prix TTC du produit : %.2f \n",p[i].ttc);
+		    printf("Prix TTC du produit : %.2f \n",p[i].prix+(p[i].prix*0.15));
 	 	    printf("\n");
 	 	    break;
-	}
-        else
-		printf("Produit introuvable."); 
+	} 
     }
+    if (code!=p[i].codeBarre){
+    	printf("Ce produit n'existe pas au stock.\n");
+	}
     
 }
 
 //Recherche par quantite
-med* qttResearch(int qtt,med *p, int nbr){
-    for(i=0;i<nbr;i++){
+void qttResearch(int qtt,med *p, int c){
+    for(i=0;i<c;i++){
         if(qtt==p[i].qtt){
         	printf("Nom du produit : %s \n",p[i].nom);
 		    printf("le codeBarre du produit : %d \n",p[i].codeBarre);
 		    printf("Prix du produit : %.2f \n",p[i].prix);
-		    printf("Prix TTC du produit : %.2f \n",p[i].ttc);
+		    printf("Prix TTC du produit : %.2f \n",p[i].prix+(p[i].prix*0.15));
 	 	    printf("\n");
-	}
-        else
-		printf("Produit introuvable.\n"); 
+	} 
     }
-    
+   
 }
 
 //Produit de quantité inferieur à 3
-med* pInf3 (med *p){
+void pInf3 (med *p,int c){
 	printf("Voici les produits inferieurs à 3 au stock : \n");
 	for(i=0;i<nbr;i++){
         if(p[i].qtt<3){
         	printf("Nom du produit : %s \n",p[i].nom);
 		    printf("le codeBarre du produit : %d \n",p[i].codeBarre);
 		    printf("Prix du produit : %.2f \n",p[i].prix);
-		    printf("Prix TTC du produit : %.2f \n",p[i].ttc);
+		    printf("Prix TTC du produit : %.2f \n",p[i].prix+(p[i].prix*0.15));
 	 	    printf("\n");
 	}
     }
@@ -135,57 +134,90 @@ med* pInf3 (med *p){
 }
 
 //la supression
-void supp (med *p){
+void supression(med *p){
+	
 	            printf("Entrer le codeBarre du produit voulu :");
 		        scanf("%d",&code);
-		        for (i=0;i<nbr;i++){
+		        for (i=0;i<c;i++){
 		        	if(code==p[i].codeBarre){
-					p[i]=p[i+1];
-		        	nbr--;
+		        		p[i]=p[i+1];
 					}
-		        	
-				}
+		        	c--;
+					}
+
+				
 				printf("Suppression bien effectue'.\n");
+				stock(p,c);
 }
 
 
 //achat
-med* achat(med *p,int n){
-	med *s;
+med* achat(med *p,int c){
+	int Nqtt;
+	
 	            printf("Entrer le codeBarre du produit voulu :");
 		        scanf("%d",&code);
-		        for(i=0;i<n;i++){
+		        for(i=0;i<c;i++){
 		        	if(code==p[i].codeBarre){
-		        	 printf("Entrer la quantite a ajouter : ");
+		        	 printf("Entrer la quantite voulu : ");
 					       scanf("%d",&Nqtt);
 					       if(Nqtt<p[i].qtt){
-					       	    j++;
 					       		p[i].qtt-=Nqtt;
-					       	    strcpy(s[i].nom,p[i].nom);
-					       	    s[i].codeBarre=p[i].codeBarre;
-					       	    s[i].qtt=Nqtt;
-					       	    s[i].prix=p[i].prix;
 					       	    printf("Produit achete' par succes.\n");
+					       	    printf("Quantite restante : %d .\n",p[i].qtt);
+					       	    printf("Le montant a payer est : %.2f .\n",(p[i].prix+(p[i].prix*0.15))*Nqtt);
+					       	    p[i].qttVendu=Nqtt;
 						   }
 						   else {
 						   printf("Ce produit est epuise du stock.\n");
 						   }
 				   }
-		        else 
-		        printf("Ce produit n'existe pas au stock.\n");
 				}		        
 }
 
+//l'ajout du quantité
+void ajout(med *p){
+	int Nqtt;
+	 printf("Entrer le codeBarre du produit voulu :");
+		        scanf("%d",&code);
+		        for(i=0;i<c;i++){
+		        	if(code==p[i].codeBarre){
+		        	       printf("Entrer la quantite a ajouter : ");
+					       scanf("%d",&Nqtt);
+					       p[i].qtt+=Nqtt;
+					   }
+				}
+				stock(p,c);
+}
+
+//statistiques
+//total des produits vendus
+void total (med *p){
+	float total;
+	for (i=0;i<c;i++){
+		total+=p[i].qttVendu*(p[i].prix+(p[i].prix*0.15));
+	}
+	 printf("Le total des ventes est : %.2f\n",total);
+}
+//moyenne des ventes 
+void moyenne(med *p,int Nqtt){
+	float total,moy;
+	for (i=0;i<c;i++){
+		total+=p[i].qttVendu*(p[i].prix+(p[i].prix*0.15));
+	}
+	moy=total/Nqtt;
+	printf("Le total des ventes est : %.2f\n",moy);
+}
+
 int main(){
-	
+	int i,k,n,m,choix1,choix2,code,qtt,Nqtt,ttl;
+    int nbr=0,j=0;
 	med *p; 
-	med *s;
-	s = malloc(j*sizeof(med));
     time_t now;
     time(&now);
     
     do {
-    	system ("cls");
+    	
     	   printf("%s",ctime(&now));
     printf("--------------------\n|       Menu       |\n--------------------\n");
     printf("|       Pour ajouter un produit, entrer 1.              |\n");
@@ -195,7 +227,8 @@ int main(){
     printf("|       Pour acheter un produit, entrer 5               |\n");
     printf("|       Pour voir les produits inferieur a 3, entrer 6. |\n");
     printf("|       Pour ajouter la quantite un produits , entrer 7 |\n");
-    printf("|       Pour Supprimer un produit, entrer 8.  |\n");
+    printf("|       Pour Supprimer un produit, entrer 8.            |\n");
+    printf("|       Pour afficher les statistiques, entrer 9.       |\n");
     printf("|           ------------------------------------------  |\n");
     printf("|       Pour quitter, entrer 0.                         |\n");
     printf("Choix : ");
@@ -203,11 +236,9 @@ int main(){
     printf("\n"); 
     switch (choix1){
     	case 0 : 
-    	        system ("cls");
     	        printf("Fin du programme, au revoir :) .");
     	break;
     	case 1 :
-    		   system ("cls");
                printf("Pour sortir appuyer sur 0.\nChoix : ");
                scanf("%d",&choix2);
                switch (choix2){
@@ -220,6 +251,7 @@ int main(){
                         nbr = 1;
                         p = malloc(nbr*sizeof(med));
                         p = saisi(p,m,nbr);
+                        c++;
                 }else{
                     m=nbr;
                     nbr+=1;
@@ -231,7 +263,7 @@ int main(){
 			   
 		break;	   
 		case 2 : 
-		        system ("cls");
+		        
 		       	printf("Enter le nombre des produits  : ");
 	            scanf("%d",&n);
 	                if(nbr==0){
@@ -239,15 +271,17 @@ int main(){
                         nbr = n;
                         p = malloc(nbr*sizeof(med));
                         p = saisi(p,m,nbr);
+                        c+=nbr;
                 }else{
                     m=nbr;
                     nbr+=n;
                     p = realloc(p,nbr);
                     p = saisi(p,m,nbr);
+                    c+=nbr;
                 }
 	    break;
 		case 3 :
-			system ("cls");
+			
 			if (nbr>0){
 			do{
 		        printf("Choisissez a methode d'affichage : \n");
@@ -255,11 +289,11 @@ int main(){
 		        scanf("%d",&choix2);
 		        switch(choix2){
 		        	case 1 : 
-		        	       sortAlpha(p,nbr);
+		        	       sortAlpha(p,c);
 		        	       printf("\n");
 		        	break;
 		        	case 2 : 
-		        	       sortPrix(p,nbr);
+		        	       sortPrix(p,c);
 		        	       printf("\n");
 		        	break;
 					
@@ -272,7 +306,7 @@ int main(){
 			printf("Le stock est encore vide. \n");
 		break;   
 		case 4 : 
-		system ("cls");
+		
 		      if (nbr>0){
 		      	do{
 		      		  printf("1: Recherche par codeBarre.\n2: Recherche par quantite' .\n");
@@ -299,23 +333,38 @@ int main(){
 			    }
 		break;
 		case 5 : 
-		system ("cls");
+		
 		      achat(p,nbr);
 		break;
 		case 6 : 
-		system ("cls");
-		        pInf3(p);
+		
+		        pInf3(p,nbr);
 		break;
 		case 7 :
-			system ("cls");
+			ajout(p);
 		        
 		break;
 		case 8 :
-			system ("cls");
-		        supp(p);
+			
+		        supression(p);
 		break ;
 		case 9 :
-			system ("cls");
+			printf("1: Pour afficher le total des prix des produits vendus en journée courante.\n");
+			printf("2: Pour afficher la moyenne des prix des produits vendus en journée courante.\n");
+			printf("3: Pour afficher le Max des prix des produits vendus en journée courante.\n");
+			printf("4: Pour afficher le Min des prix des produits vendus en journée courante.\n");
+			printf("0: Pour quitter.\n");
+			printf("choix : ");
+			scanf("%d",&choix2);
+			switch(choix2){
+				case 1 : 
+				       total (p);
+				      
+				break; 
+				case 2 :
+				       moyenne(p,Nqtt);
+				break;       
+			}
 		break;            
 	}
 }
